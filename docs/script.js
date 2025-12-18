a5b2ff15e09ec12970b3d48f7571650eb643f45c
felipe.mariano <andrefelipesouzamariano25@gmail.com>
2025-12-18 09:49:05 -0300

    ok

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,15 @@
+// define duas constantes que chamam doi id, assim, se algo for moadifcado nelas aqui também muda.
 const botao_text = getElementById("abrir-texto");
-const  caixa_texto = getElementById("caixa_texto");
+const caixa_texto = getElementById("caixa_texto");
 
+
+// quando o botão for crilado faz o evento dentro de ().
 botao_text.addEventListener("click", () => {
-
-if(caixa_texto.style.display === "block"){
-caixa_texto.style.display = "none";
-} else {
-  caixa_texto.style.display = "block"; }
+// se caixa de texto estiver a mostra então aquando botão for clicado ela escondera e vice versa.
+    if (caixa_texto.style.display === "block") {
+        caixa_texto.style.display = "none";
+    } else {
+        caixa_texto.style.display = "block";
+    }
 
 })
