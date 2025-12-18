79a03ea9c5694f52d27edccfd1015265d66ae79a
felipe.mariano <andrefelipesouzamariano25@gmail.com>
2025-12-18 09:19:43 -0300

    button and text area

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+const botao_text = getElementById("abrir-texto");
+const  caixa_texto = getElementById("caixa_texto");
+
+botao_text.addEventListener("click", () => {
+
+  caixa_texto.style.display = "block";
+})
