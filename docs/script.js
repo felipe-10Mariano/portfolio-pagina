c70a378c46b40df08c683aa8a3871f8e2443e83d
felipe.mariano <andrefelipesouzamariano25@gmail.com>
2025-12-21 10:34:39 -0300

    caixa seção

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,5 @@
 // define duas constantes que chamam doi id, assim, se algo for moadifcado nelas aqui também muda.
-const botao_chama_text = document.getElementById("abrir_texto");
+const botao_seccao = document.getElementById("abrir_texto");
 const caixa_texto = document.getElementById("caixa_texto");
 const botao_envia = document.querySelector(".envio_texto");
 const botao_section = document.querySelector("section");
@@ -7,13 +7,17 @@ const botao_section = document.querySelector("section");
 caixa_texto.style.display === "none";
 
 // quando o botão for crilado faz o evento dentro de ().
-botao_chama_text.addEventListener("click", () => {
+
+botao_seccao.addEventListener("click", () => {
     // verifica se a caixa de texto está escondida ou a amotra.
-    if (caixa_texto.style.display === "none") {
-            caixa_texto.style.display = "block";
-        } else {
-            caixa_texto.style.display = "none";
-        }
+   
+   
+   
+    // if (caixa_texto.style.display === "none") {
+    //         caixa_texto.style.display = "block";
+    //     } else {
+    //         caixa_texto.style.display = "none";
+    //     }
 })
 
 botao_envia.addEventListener("click", () => {
